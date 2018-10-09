fe043c21eabfbfe629a3aea5c4676e4f683a8c78
Admin <admin@FerfromHell.local>
2018-10-09 13:02:20 -0700

    Gret new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,11 +16,18 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket) => {
     console.log('New user connected');
-//     socket.emit('newMessage',{
-//         from: 'John',
-//         text: 'See you then',
-//         createdAt: 123123
-//    });
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+   });
+   // socket.broadcast.emit from Admin text New user joined
+   socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
    socket.on('createMessage', (message) => {
         console.log('createMessage',message);
         io.emit('newMessage',{
@@ -28,6 +35,11 @@ io.on('connection',(socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        // });
    });
    socket.on('createEmail',(newEmail) => {
         console.log('createEmail', newEmail);
